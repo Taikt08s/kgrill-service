41998611336da2dec63ef8a121e3d551afd3185e
Dang Dinh Tai (K17 HCM) <taiddse173059@fpt.edu.vn>
2024-06-26 23:03:11 +0700

    decrypt jwt hotfix

--- src/main/java/com/swd392/group2/kgrill_service/config/JwtAuthenticationFilter.java
+++ src/main/java/com/swd392/group2/kgrill_service/config/JwtAuthenticationFilter.java
@@ -40,7 +40,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
-        jwtToken = authHeader.substring(7);
+        jwtToken = authHeader.substring(7).trim();
 //        userEmail = jwtService.extractUsername(jwtToken);
 
         Claims claims = null;
