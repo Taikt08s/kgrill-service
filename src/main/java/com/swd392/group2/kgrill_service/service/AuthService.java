8a05e67d64d243f7811beb75a62c7615ceb934b2
Dang Dinh Tai (K17 HCM) <taiddse173059@fpt.edu.vn>
2024-06-26 18:30:35 +0700

    encrypted jwt (test)

--- src/main/java/com/swd392/group2/kgrill_service/service/AuthService.java
+++ src/main/java/com/swd392/group2/kgrill_service/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.swd392.group2.kgrill_service.service;
 
+import com.nimbusds.jose.JOSEException;
 import com.swd392.group2.kgrill_service.dto.*;
 
 import jakarta.mail.MessagingException;
@@ -12,7 +13,7 @@ import java.io.UnsupportedEncodingException;
 public interface AuthService {
     void register(RegistrationRequest request) throws MessagingException, UnsupportedEncodingException;
 
-    AuthenticationResponse authenticate(AuthenticationRequest request);
+    AuthenticationResponse authenticate(AuthenticationRequest request) throws JOSEException;
 
     void activateAccount(String token, HttpServletResponse response) throws MessagingException, UnsupportedEncodingException;
 
