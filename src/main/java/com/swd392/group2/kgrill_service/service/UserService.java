a178339fbfb6977a03198d026dc62ce6771504a0
Dang Dinh Tai (K17 HCM) <taiddse173059@fpt.edu.vn>
2024-06-27 18:37:30 +0700

    decrypt jwt hotfix

--- src/main/java/com/swd392/group2/kgrill_service/service/UserService.java
+++ src/main/java/com/swd392/group2/kgrill_service/service/UserService.java
@@ -1,14 +1,16 @@
 package com.swd392.group2.kgrill_service.service;
 
+import com.nimbusds.jose.JOSEException;
 import com.swd392.group2.kgrill_service.dto.UserProfileResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import com.swd392.group2.kgrill_service.dto.UserProfileDto;
 
+import java.text.ParseException;
 import java.util.UUID;
 
 public interface UserService {
-    ResponseEntity<Object> getUserInformation(HttpServletRequest request);
+    ResponseEntity<Object> getUserInformation(HttpServletRequest request) throws ParseException, JOSEException;
 
     UserProfileResponse getAllUsers(int pageNo, int pageSize, String sortBy, String sortDir, String email);
 
