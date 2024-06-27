c88c23e7c81ca8ed03b0fe1d7e87675b26d04509
Dang Dinh Tai (K17 HCM) <taiddse173059@fpt.edu.vn>
2024-06-27 17:58:34 +0700

    decrypt jwt hotfix

--- src/main/java/com/swd392/group2/kgrill_service/service/impl/JwtImplement.java
+++ src/main/java/com/swd392/group2/kgrill_service/service/impl/JwtImplement.java
@@ -38,13 +38,26 @@ public class JwtImplement implements JwtService {
     private String jwtIssuer;
 
     @Override
-    public String extractUsername(String jwtToken) throws ParseException, JOSEException {
-        return extractClaim(jwtToken, Claims::getSubject);
+    public String extractUsername(String jwtToken) {
+//        return extractClaim(jwtToken, Claims::getSubject);
+        try {
+            return extractClaim(jwtToken, Claims::getSubject);
+        } catch (ParseException | JOSEException e) {
+            // Handle the exception or rethrow a custom exception
+            throw new RuntimeException("Error extracting username from JWT token", e);
+        }
     }
 
     private <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver) throws ParseException, JOSEException {
-        final Claims claims = extractAllClaims(jwtToken);
-        return claimsResolver.apply(claims);
+//        final Claims claims = extractAllClaims(jwtToken);
+//        return claimsResolver.apply(claims);
+        try {
+            final Claims claims = extractAllClaims(jwtToken);
+            return claimsResolver.apply(claims);
+        } catch (ParseException | JOSEException e) {
+            // Handle the exception or rethrow a custom exception
+            throw new RuntimeException("Error extracting claim from JWT token", e);
+        }
     }
 
     private Claims extractAllClaims(String token) throws ParseException, JOSEException {
