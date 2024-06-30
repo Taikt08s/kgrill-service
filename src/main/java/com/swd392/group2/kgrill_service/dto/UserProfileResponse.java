2cbf5d13dfc331fd3c68043c8b241d9b6c61c1f4
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2024-06-30 23:45:31 +0700

    refactor(admin): get/update

--- src/main/java/com/swd392/group2/kgrill_service/dto/UserProfileResponse.java
+++ src/main/java/com/swd392/group2/kgrill_service/dto/UserProfileResponse.java
@@ -13,16 +13,18 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserProfileResponse {
+
+    @JsonProperty(value = "content", index = 1)
     private List<CustomUserProfile> content;
 
-    @JsonProperty(value = "page_no", index = 1)
+    @JsonProperty(value = "page_no", index = 2)
     private int pageNo;
-    @JsonProperty(value = "page_size", index = 2)
+    @JsonProperty(value = "page_size", index = 3)
     private int pageSize;
-    @JsonProperty(value = "total_elements", index = 3)
+    @JsonProperty(value = "total_elements", index = 4)
     private long totalElements;
-    @JsonProperty(value = "total_pages",index = 4)
+    @JsonProperty(value = "total_pages",index = 5)
     private int totalPages;
-    @JsonProperty(value = "last", index = 5)
+    @JsonProperty(value = "last", index = 6)
     private boolean last;
 }
