be1ed5b687a7a3f2db72460975e510ff94522c8a
BuiDuyKhang2003 <huvo201223@gmail.com>
2024-07-03 22:35:25 +0700

    update crud package method on browser and mobile

--- src/main/java/com/swd392/group2/kgrill_service/service/PackageService.java
+++ src/main/java/com/swd392/group2/kgrill_service/service/PackageService.java
@@ -1,6 +1,7 @@
 package com.swd392.group2.kgrill_service.service;
 
 import com.swd392.group2.kgrill_service.dto.request.PackageRequest;
+import com.swd392.group2.kgrill_service.dto.response.PackageDetailResponseForMobile;
 import com.swd392.group2.kgrill_service.dto.response.PackageResponseForAdminAndManager;
 import com.swd392.group2.kgrill_service.dto.response.PackageResponseForMobileAsList;
 import org.springframework.data.domain.Page;
@@ -20,4 +21,6 @@ public interface PackageService{
     Page<PackageResponseForAdminAndManager> getAllPackagePages(int pageNumber, int pageSize, String sortField, String sortDir);
 
     List<PackageResponseForMobileAsList> getAllPackageOnMobile();
+
+    PackageDetailResponseForMobile getPackageDetailOnMobile(int id);
 }
