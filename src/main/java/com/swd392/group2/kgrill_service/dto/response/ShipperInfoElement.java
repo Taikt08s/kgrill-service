ebbae99d933b4d533f4ea7f05dff92f107e1f1a5
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2024-07-12 15:39:24 +0700

    feat(Shipper): add reponse model

---
+++ src/main/java/com/swd392/group2/kgrill_service/dto/response/ShipperInfoElement.java
@@ -0,0 +1,35 @@
+package com.swd392.group2.kgrill_service.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Access;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "ShipperInfo element object response for shipperInfoResponse")
+public class ShipperInfoElement {
+
+    @Schema(description = "Shipper's ID", example = "1")
+    private int id;
+
+    @Schema(description = "Shipper's name", example = "Nguyễn Văn A")
+    private String name;
+
+    @Schema(description = "Shipper's total order", example = "20")
+    @JsonProperty(value = "total_order")
+    private int totalOrder;
+
+    @Schema(description = "Shipper's completed order", example = "10")
+    @JsonProperty(value = "completed_order")
+    private int completedOrder;
+
+    @Schema(description = "Shipper's cancelled order", example = "10")
+    @JsonProperty(value = "cancelled_order")
+    private int cancelledOrder;
+}
