38d872607631f9058bf6587751fc3b0a2351226f
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2024-07-19 13:31:28 +0700

    refactor(DeliveryOrder): change @JsonPropery in DeliveryOrderElement

--- src/main/java/com/swd392/group2/kgrill_service/dto/response/DeliveryOrderElement.java
+++ src/main/java/com/swd392/group2/kgrill_service/dto/response/DeliveryOrderElement.java
@@ -3,10 +3,7 @@ package com.swd392.group2.kgrill_service.dto.response;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,43 +15,47 @@ import java.util.UUID;
 @Schema(name = "DeliveryOrderElement", description = "Delivery Order element that return user ordering for Manager")
 public class DeliveryOrderElement {
 
-    @JsonIgnore
     @Schema(name = "id", description = "Delivery Order ID")
+    @JsonProperty(value = "Delivery_Order_Id", index = 1)
     private Integer id;
 
     @JsonIgnore
     private UUID accountId;
 
     @Schema(name = "User's ame", description = "Customer Name")
-    @JsonProperty("User_name")
+    @JsonProperty(value = "User_name", index = 2)
     private String userName;
 
     @Schema(name = "user's Phone", description = "Customer Phone")
+    @JsonProperty(value = "Phone", index = 3)
     private String Phone;
 
-    @JsonProperty(value = "Package_name")
+    @JsonProperty(value = "Package_name", index = 4)
     @Schema(description = "Package's name", example = "Combo Bò nướng mĩ vị Tailor")
     private List<String> packageName;
 
     @Schema(name = "orderValue", description = "Total Price")
-    @JsonProperty("Order_value")
+    @JsonProperty(value = "Order_value", index = 5)
     private float orderValue;
 
     @Schema(description = "User's address", example = "123 Main St, Springfield")
+    @JsonProperty(value = "Address", index = 6)
     private String address;
 
     @Schema(description = "Latitude of the address", example = "21.123456")
+    @JsonProperty(value = "Latitude", index = 7)
     private Double latitude;
 
     @Schema(description = "Longitude of the address", example = "105.123456")
+    @JsonProperty(value = "Longitude", index = 8)
     private Double longitude;
 
     @Schema(name = "orderDate", description = "Order Date")
-    @JsonProperty("Order_date")
+    @JsonProperty(value = "Order_date" , index = 9)
     private String orderDate;
 
     @Schema(name = "orderStatus", description = "Order Status")
-    @JsonProperty("Order_status")
+    @JsonProperty(value = "Order_status", index = 10)
     private String orderStatus;
 
 
